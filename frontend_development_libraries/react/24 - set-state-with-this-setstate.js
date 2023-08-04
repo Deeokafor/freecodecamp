25a7ffa47c001ea8232af9d37ccbe2a5dd19b55d
Divine Chukwunwemmeri Okafor-udah [~Dee] <okafor.udahdivine@gmail.com>
2023-08-04 09:18:29 +0100

    consolidate react set state

--- frontend_development_libraries/react/24 - set-state-with-this-setstate.js
+++ frontend_development_libraries/react/24 - set-state-with-this-setstate.js
@@ -24,7 +24,6 @@ class MyComponent extends React.Component {
 };
 
 {/*Divine's Practice Below */}
-
 class MyCmp extends React.Component {
   constructor(props) {
     super(props);
@@ -33,13 +32,11 @@ class MyCmp extends React.Component {
     };
     this.handleClick = this.handleClick.bind(this);
   }
-
   handleClick () {
     this.setState({
       string: 'string change!'
     })
   }
-
   render() {
     return(
       <React.Fragment>
@@ -49,15 +46,70 @@ class MyCmp extends React.Component {
     )
   }
 }
+ReactDOM.render(<MyCmp />, document.querySelector('#challenge-node'));
 
-const drender = (elem, target) => {
-  ReactDOM.render(elem, target);
+{/* practise 2 */}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'user',
+      date: 'dd-mm-yy'
+    }
+    this.getUser = this.getUser.bind(this);
+  }
+  getUser () {
+    this.setState({
+      user: 'Divine',
+      date: '04-August-2023'
+    })
+  }
+  render() {
+    return(
+      <div>
+        <p>{this.state.user} joined {this.state.date}
+        </p>
+        <button onClick={this.getUser}>get user</button>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.querySelector('#challenge-node'));
+
+// practise 3
+class App_2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appname: "App Name",
+      year_produced: "dd-mm-yy",
+      author: "Author"
+    }
+    this.getApp = this.getApp.bind(this);
+  }
+
+  getApp () {
+    this.setState({
+      app_name: <i>"Freecodecamp React Curriculum Practice"</i>,
+      year_produced: "2023-08-04",
+      author: "Okafor-udah Divine"
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <h3>App: {this.state.app_name}</h3>
+        <p>Author: {this.state.author}</p>
+        <p>Build Year: {this.state.year_produced}</p>
+
+        <button onClick={this.getApp}>Show Details</button>
+      </div>
+    )
+  }
 }
 
-drender(<MyCmp />, document.querySelector('#challenge-node'))
-
-/* practise 2 */
-
+ReactDOM.render(<App_2 />, document.getElementById('challenge-node'));
 
 
 
